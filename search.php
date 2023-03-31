00eeecf4b0bd716f8d5642e97e3417c9ecc48fce
Hensley Pierre <e6133965@cmaisonneuve.qc.ca>
2023-03-31 09:07:28 -0400

    changement des pages search-4w4.php,search-cours.php et search.php pour l'affichage par défaut

--- search.php
+++ search.php
@@ -7,16 +7,13 @@
 <?php get_header(); ?>
 <main>
 <h3>Résultats de la recherche</h3>
-    <?php
+<?php
     if(have_posts()):
-        while (have_posts()) : the_post();
-         the_title('<h1>', '</h1>'); ?>
-        
-         <?= wp_trim_words(get_the_excerpt(), 50, "[...]"); ?>
-         <hr>
-        <?php  endwhile;
-endif;
-?>
+        while (have_posts()) : the_post(); 
+        $ma_categorie = '4w4';
+            get_template_part("template-parts/search", $ma_categorie);
+      endwhile;
+endif;?>
 
 
 </main>
---
+++ template-parts/search-cours.php
@@ -0,0 +1,18 @@
+<?php
+/***
+ * Template part pour afficher un article cours 4w4
+ */
+     $titre = get_the_title(); 
+      $sigle = substr($titre,0,7); // 582-1w1 - Mise en page web
+      $titre_long = substr($titre,7,-5); // 582-1w1 Mise en ... (60h)
+      $duree = substr($titre, strpos($titre, '('));
+      // strpos($titre, '(') permet de trouver la position du caractère ,'('
+     ?>
+     <article class="blocflex__article">
+     <h3><a href="<?php the_permalink(); ?>"> <?= $sigle ?></a></h3>
+     <h5><?= $titre_long ?></h5>
+     <p><?= wp_trim_words(get_the_excerpt(), 15); ?></p>
+     <h5><?= $duree ?></h5>
+     <p><?php the_field('enseignant')?></p>
+     <p><?php the_field('domaine')?></p>
+            </article>
