3081bb2147db611787c680995ad29c732666a1e2
Hensley Pierre <e6133965@cmaisonneuve.qc.ca>
2023-03-31 10:15:02 -0400

    changement de la page search.php

--- search.php
+++ search.php
@@ -7,14 +7,17 @@
 <?php get_header(); ?>
 <main>
 <h3>Résultats de la recherche</h3>
+<section class="blocflex_search">
 <?php
     if(have_posts()):
         while (have_posts()) : the_post(); 
         $ma_categorie = '4w4';
             get_template_part("template-parts/search", $ma_categorie);
+            the_excerpt('<p>', '</p>');
+            
       endwhile;
 endif;?>
 
-
+</section>
 </main>
 <?php get_footer(); ?>
