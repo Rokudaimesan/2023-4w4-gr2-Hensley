c531c3b3652d7bf70147506e7a0d1448ee9fc545
Hensley Pierre <e6133965@cmaisonneuve.qc.ca>
2023-04-21 11:00:22 -0400

    ajout de deuxieme video sur le footer

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
 <footer class="site__footer">
    <section><?php the_custom_logo(); ?></section>
-   <section>Colonne 2</section>
+   <section>
+   <div class="sidebar">
+         <?php dynamic_sidebar('footer_2');?>
+   </section>
    <section>
       <div class="sidebar">
-         <?php dynamic_sidebar('footer1');?>
+         <?php dynamic_sidebar('footer_1');?>
       </div>
    </section>
 </footer>
